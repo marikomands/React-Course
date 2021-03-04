ab133bbff633570d07edcb192706c03094c07bc4
Mariko Mands <marikomands@gmail.com>
2021-03-04 11:39:32 +0000

    Pet Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
-
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
     React.createElement("h1", {}, "Adopt Me!"),
